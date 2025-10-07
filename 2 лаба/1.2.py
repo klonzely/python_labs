ccdcef6e8b9ee6af3273def81821698d468655de
klonzely <l.turovsky@yandex.ru>
2025-10-07 09:05:47 +0300

    k

--- 2 лаба/1.2.py
+++ 2 лаба/1.2.py
@@ -1,9 +1,8 @@
-m=[[1, 2, 3]]
-def transpose(mat: list[list[float | int]]) -> list[list]:
-    if not m:
-        raise ValueError("Список пустой")
-    else:
-        q=[]
-        q.append(zip(m))
-        return q
-print(transpose(m))
+m =[3, 1, 2, 1, 3]
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    return sorted(set(m))
+print(unique_sorted(m))
+
+    
+    
+    
