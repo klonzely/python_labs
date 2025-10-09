0de17c5356744397245562ae291579ff7bf94db1
klonzely <l.turovsky@yandex.ru>
2025-10-09 17:39:06 +0300

    k

--- 2 лаба/3.py
+++ 2 лаба/3.py
@@ -1,12 +1,11 @@
-# inp = ('иванов иван иванович', 'BIVT-25', 4.6)
-# def format_record(rec: tuple[str, str, float]) -> str:
-#     """Некорректные записи -> ValueError"""
-#     fio, group, gpa = inp[0], inp[1], inp[2]
-#     if fio == '' or group == '' or type(gpa) != float:
-#         return 'ValueError'
-#     fio = fio.split()
-#     for i in range(len(fio)):
-#         fio[i] = fio[i][0].upper() + fio[i][1:]
-#     return f'{fio[0]} {fio[1][0]}.{fio[2][0]}., гр. {group}, GPA {gpa:.2f}'
-# print(format_record(inp))
-print('иванов иван иванович'.split())
+inp = ('иванов иван иванович', 'BIVT-25', 4.6)
+def format_record(rec: tuple[str, str, float]) -> str:
+    """Некорректные записи -> ValueError"""
+    fio, group, gpa = inp[0], inp[1], inp[2]
+    if fio == '' or group == '' or type(gpa) != float:
+        return 'ValueError'
+    fio = fio.split()
+    for i in range(len(fio)):
+        fio[i] = fio[i][0].upper() + fio[i][1:]
+    return f'{fio[0]} {fio[1][0]}.{fio[2][0]}., гр. {group}, GPA {gpa:.2f}'
+print(format_record(inp))
